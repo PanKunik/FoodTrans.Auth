607c700d5710fea5e751d3d0e7ae232a01f62ecc
PanKunik <patryk.kunicki@pm.me>
2023-03-06 20:34:31 +0100

    Adding unit tests for FirstName VO

--- src/FoodTrans.Auth.Domain/Users/ValueObjects/FirstName.cs
+++ src/FoodTrans.Auth.Domain/Users/ValueObjects/FirstName.cs
@@ -17,21 +17,14 @@ public sealed class FirstName : ValueObject
 
     public static ErrorOr<FirstName> Create(string firstName)
     {
-        var errors = new List<Error>();
-
         if (string.IsNullOrWhiteSpace(firstName))
         {
-            errors.Add(Errors.Auth.EmptyFirstName);
+            return Errors.Auth.EmptyFirstName;
         }
 
-        if (firstName.Length < 3 || firstName.Length > 50)
+        if (firstName.Length < 2 || firstName.Length > 50)
         {
-            errors.Add(Errors.Auth.InvalidFirstNameLength);
-        }
-
-        if (errors.Count > 0)
-        {
-            return errors;
+            return Errors.Auth.InvalidFirstNameLength;
         }
 
         return new FirstName(firstName);
